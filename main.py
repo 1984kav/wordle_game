4f4cedc5196051a30024b59302bf9c925c4d3e7a
1984kav <1984kav@gmail.com>
2022-10-18 11:06:16 +0300

    extend dictionaries and small visual changes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from random import choice as random_choice
-from words_5 import words_5, all_words_5
+from words_5 import popular_words_5, all_words_5
 
 
 def color_print(test_word, computer_word):
     def red_print(text):
-        print('\033[31m', text, '\033[0m', sep='', end='')
+        print('\033[1;31m', text.upper(), '\033[0m', sep='', end=' ')
 
     def green_print(text):
-        print('\033[32m', text, '\033[0m', sep='', end='')
+        print('\033[1;32m', text.upper(), '\033[0m', sep='', end=' ')
 
     def yellow_print(text):
-        print('\033[33m', text, '\033[0m', sep='', end='')
+        print('\033[1;33m', text.upper(), '\033[0m', sep='', end=' ')
 
     dict_letter_word = {letter: computer_word.count(letter) for letter in set(computer_word)}
 
@@ -28,7 +28,7 @@ def color_print(test_word, computer_word):
     print('\033[0m', sep='', end='\n')
 
 
-word = random_choice(words_5)
+word = random_choice(popular_words_5)
 attempt = 0
 print(f'Я загадал слово их {len(word)} букв...\n')
 
